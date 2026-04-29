bc810ab8334c91c8260cc969302b31784ced53e3
lumber-3150 <xig3150@gmail.com>
2026-04-29 15:51:21 +0900

    課題修正

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -5,7 +5,8 @@ public class Car_Chapter15 {
 	private int speed	= 10;
 		
 	public void changeGear(int afterGear) {
-		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+		this.gear = afterGear;
+		System.out.println("ギア" + gear + "から" + this.gear + "に切り替えました");
 		switch(afterGear) {		
 		case 1:
 			this.speed	= 10;
